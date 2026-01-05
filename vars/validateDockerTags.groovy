52dc15a58e181e77a8e975d392521dfa144e48f4
nikhil23407 <116107589+nikhil23407@users.noreply.github.com>
2026-01-05 13:35:20 +0530

    Delete vars/validateDockerTags.groovy

--- vars/validateDockerTags.groovy
+++
@@ -1,5 +0,0 @@
-def call(Map params) {
-    if (!params.FRONTEND_DOCKER_TAG?.trim() || !params.BACKEND_DOCKER_TAG?.trim()) {
-        error("FRONTEND_DOCKER_TAG and BACKEND_DOCKER_TAG must be provided.")
-    }
-}
